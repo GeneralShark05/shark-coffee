ffc188ff2755649517e04841b4f2aac52c57605d
GeneralShark05 <125304288+GeneralShark05@users.noreply.github.com>
2023-04-04 10:53:35 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,7 +5,8 @@ local blend = {
 	'Black Coffee', 'Coffee with Milk', 'Coffee with Sugar', 'Double Double'
 }
 
-RegisterServerEvent('sharkcoffee-coffee', function(type)
+RegisterServerEvent('sharkcoffee-coffee', function(type)	
 	ox_inventory:RemoveItem(source, 'money', 3)
+	Wait(2000)
 	ox_inventory:AddItem(source, 'coffee', 1, {label = blend[type], description = 'A '..blend[type]})
 end)
